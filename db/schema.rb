daaf571ec1a82420838e05a1e7c1dfbfb2b4ebac
Ziv Zamechek <ziv921@yahoo.com>
2016-05-09 23:43:40 -0400

    Link Reviews to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504220927) do
+ActiveRecord::Schema.define(version: 20160510033441) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160504220927) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
