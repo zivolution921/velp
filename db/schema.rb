7e6fe4c918e5b115feaf591d59c11edf5493a0a8
Ziv Zamechek <ziv921@yahoo.com>
2016-05-11 12:56:31 -0400

    Link to Reviews with Restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510033441) do
+ActiveRecord::Schema.define(version: 20160511163922) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -32,9 +32,10 @@ ActiveRecord::Schema.define(version: 20160510033441) do
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
     t.text     "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "user_id"
+    t.integer  "restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
